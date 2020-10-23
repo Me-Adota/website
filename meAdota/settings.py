f6ae3d5ef81c1ffbe01c829b5de60107c7c426d1
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-10-23 13:31:13 -0300

    :arrow_up: Update settings.py

--- meAdota/settings.py
+++ meAdota/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'meAdota.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR / "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
