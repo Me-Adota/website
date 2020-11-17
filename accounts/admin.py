9482067435d8c941b20cd126eb1a443d0bdcd711
Douglas-Castro <farias.douglascastro@gmail.com>
2020-11-17 14:17:47 -0300

    :bulb: Add accounts app

---
+++ accounts/admin.py
@@ -0,0 +1,44 @@
+# accounts.admin.py
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm  #update view
+    add_form = UserAdminCreationForm    #create view
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin')
+    list_filter = ('admin','staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email','full_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
---
+++ accounts/models.py
@@ -0,0 +1,100 @@
+# accounts.models.py
+
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+# accounts.models.py
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+        if not full_name:
+            raise ValueError("Users must have a full_name")
+        user_obj = self.model(
+            email = self.normalize_email(email),
+            full_name=full_name
+        )
+        user_obj.set_password(password) # change user password
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email,full_name, password=None):
+        user = self.create_user(
+                email,
+                full_name=full_name,
+                password=password,
+                is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, full_name, password=None):
+        user = self.create_user(
+                email,
+                full_name=full_name,
+                password=password,
+                is_staff=True,
+                is_admin=True
+        )
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False) # a admin user; non super-user
+    admin = models.BooleanField(default=False) # a superuser
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    # notice the absence of a "Password field", that is built in.
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name'] # Email & Password are required by default.
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their e mail address
+        return self.email
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+    @property
+    def is_active(self):
+        "Is the user active?"
+        return self.active
