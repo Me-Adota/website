c69f1b89a9f636191e74877af02c9b39d11ed96d
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-11-17 16:40:28 -0300

    :bulb: Add loading animation

---
+++ static/js/loading.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    setTimeout(function () {
+        $("#loading-box").addClass("loaded");
+    }, 2000);
+
+    setTimeout(function () {
+        $("#loading-box").addClass("loaded2");
+    }, 2500);
+});
