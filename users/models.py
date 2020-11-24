da8c87d55e29ffc861b64d01407f590728bcdd06
GuilhermeBraz <gvcerveirabraz@gmail.com>
2020-11-23 23:53:24 -0300

    :arrow_up: Update models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 
-# Create your models here.
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from cpf_field.models import CPFField
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
@@ -51,12 +53,24 @@ class User(AbstractBaseUser):
         verbose_name='email address',
         max_length=255,
         unique=True,
+        null =True,
     )
     full_name = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False) # a admin user; non super-user
     admin = models.BooleanField(default=False) # a superuser
     timestamp   = models.DateTimeField(auto_now_add=True)
+    cpf = CPFField('cpf', null= True)
+    date_of_birth = models.DateField(verbose_name=_("Date of birth"), blank=True, null=True)
+    address1 = models.CharField(verbose_name=_("Address line 1"), max_length=1024, blank=True, null=True)
+    address2 = models.CharField(verbose_name=_("Address line 2"), max_length=1024, blank=True, null=True)
+    zip_code = models.CharField(verbose_name=_("Postal Code"), max_length=12, blank=True, null=True)
+    city = models.CharField(verbose_name=_("City"), max_length=1024, blank=True, null=True)
+    country = CountryField(blank=True, null=True)
+    phone_regex = RegexValidator(regex=r"^\+(?:[0-9]●?){6,14}[0-9]$", message=_("Enter a valid international mobile phone number starting with +(country code)"))
+    mobile_phone = models.CharField(validators=[phone_regex], verbose_name=_("Mobile phone"), max_length=17, blank=True, null=True)
+    additional_information = models.CharField(verbose_name=_("Additional information"), max_length=4096, blank=True, null=True)
+    photo = models.ImageField(verbose_name=_("Photo"), upload_to='photos/', default='photos/default-user-avatar.png')
     # notice the absence of a "Password field", that is built in.
 
     USERNAME_FIELD = 'email'
@@ -64,7 +78,7 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
 
-    def get_username(self):
+    def get_full_name(self):
         # The user is identified by their email address
         return self.full_name
 
