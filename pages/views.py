54d42c75067722249ab10e328af3abfde536be21
GuilhermeBraz <gvcerveirabraz@gmail.com>
2020-12-01 16:10:48 -0300

    :arrow_up:Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from pets.models import Pet
+from users.models import User
 from pets.filters import *
+
 
 def aboutUs(request):
     return render(request, 'pages/about_us.html' )
@@ -19,10 +21,23 @@ def makeafriend(request):
 
 def HomeView(TemplateView):
     pets = Pet.objects.all()
+    pets = pets.filter(isAdopted = False)[:6]
+
     return render(TemplateView, "pages/index.html",  {'pets' : pets})
 
 def petDetails(request,id):
+    logged = True
+    if not request.user.is_authenticated:
+        logged = False
     pet = Pet.objects.get(pk=id)
     pet.save()
-    return render(request, 'pages/petDetails.html', {'pet':pet})
+
+    user = User.objects.get(id=pet.user_id)
+    user.save()
+    try:
+        user.mobile_phone = user.mobile_phone.replace('-','').replace('+','')
+    except:
+        pass
+
+    return render(request, 'pages/petDetails.html', {'pet':pet, 'user':user, 'logged':logged})
 
