80b926c497aabd677c00641b81626bad09e8ca6e
GuilhermeBraz <gvcerveirabraz@gmail.com>
2020-12-01 00:06:43 -0300

    :bug:Fix urls starting with /

--- pets/urls.py
+++ pets/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'pets'
 urlpatterns = [
     path('', views.All, name="userInfo"),
-    path('/insert', views.insertPets, name="insertPet"),
-    path('/my', views.userPets, name="mypets"),
-    path('/delete/<int:id>>', views.petDelete, name="deletePet"),
-    path('/update/<int:id>', views.editPet, name="EditPet"),
+    path('insert', views.insertPets, name="insertPet"),
+    path('my', views.userPets, name="mypets"),
+    path('delete/<int:id>>', views.petDelete, name="deletePet"),
+    path('update/<int:id>', views.editPet, name="EditPet"),
 
 ]
