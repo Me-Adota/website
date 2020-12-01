07aa6bf1ee7f4db72d31f102b4b3f8a8429b6ad5
GuilhermeBraz <gvcerveirabraz@gmail.com>
2020-12-01 00:05:36 -0300

    :bug:Fix urls starting with /

--- pages/urls.py
+++ pages/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'pages'
 urlpatterns = [
-    path('', views.HomeView),    
+    path('', views.HomeView),
     path('aboutus', views.aboutUs, name="aboutUs"),
     path('informations', views.infos, name="infos"),
     path('home', views.HomeView, name = "home"),
-    path('makeafriend', views.makeafriend, name = 'makeafriend'),    
-    path('/details/<int:id>', views.petDetails, name="Details"),
+    path('makeafriend', views.makeafriend, name = 'makeafriend'),
+    path('details/<int:id>', views.petDetails, name="Details"),
 ]
