243ceb9338ca44bfded21c77c2ff9e8cf1908bbd
Guilherme Peixoto <guizinpeix7@gmail.com>
2020-12-03 03:34:40 -0300

    :bug: More bugs corrected in url

--- meAdota/urls.py
+++ meAdota/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('system/personal/', include('users.urls')),
     path('system/pets/', include('pets.urls')),
 ]
